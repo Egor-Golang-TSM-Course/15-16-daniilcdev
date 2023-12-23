3ffe9d58d2ee6650c44920398f6bb03834005d9b
Daniil Cherepkov <groznybear@gmail.com>
2023-12-23 11:09:12 +0300

    fix: comment back comment line

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	cancelcallstack "server-context/cancelCallStack"
+	cancelwebrequest "server-context/cancelWebRequest"
 	"time"
 )
 
@@ -16,11 +17,12 @@ func main() {
 		"https://linkedin.com",
 	}
 
-	task 1
+	// task 1
 	cancelwebrequest.RequestUrlsWithTimeout(
 		context.Background(),
 		urls,
 		400*time.Millisecond)
 
+	// task 2
 	cancelcallstack.CallAndCancel(context.Background(), 100*time.Millisecond)
 }
